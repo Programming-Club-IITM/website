8b451793cae6a5d31afc224b4efdf4ef49ebc506
Programming-Club-IITM <153910718+Programming-Club-IITM@users.noreply.github.com>
2026-08-15 15:36:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/prog-club-website-v4/',
+    base: '/website/',
 })
 
